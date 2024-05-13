03c512108a0ab7fca7641da2ad53f5787b545499
Chris <oscar.sletvold@gmail.com>
2024-05-13 09:55:04 +0200

    Add project files.

---
+++ IntroToValueTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿using IntroToValueTypes;
+
+class Program
+{
+    static void Main()
+    {
+        // executes the static methods directly
+        decimal NewNumber = ValueTypes.RunNumbers();
+        int NewNumber2;
+        NewNumber2 = ValueTypes.RunNumbers2();
+        string NewString = ValueTypes.RunText();
+
+        // should display both outputs in the console
+        Console.WriteLine(NewNumber);
+        Console.WriteLine(NewNumber2);
+        Console.WriteLine(NewString); // Should output "This is a string"
+    }
+}
---
+++ IntroToValueTypes/ValueTypes.cs
@@ -0,0 +1,44 @@
+﻿namespace IntroToValueTypes;
+
+internal class ValueTypes
+{
+    public int Number = 1;
+    long noe = 0;
+
+    float desimalTall = 1.0f; //nøyaktig
+    double desimalTall1 = 1.01;
+    decimal desimalTall3 = 1.01M; //veldig nøyaktig
+
+    char bokstav = 'a';
+    string tekst = "hei";
+
+    bool santUsant = false;
+
+    private int[] tallArray = { 1, 2, 3 };
+    private string[] tekstArray = { "hei", "på", "deg" };
+
+    List<int> listeMedTall = new List<int>(); //lager en ny liste med tall
+
+    public static decimal RunNumbers()
+    {
+        int a = 5;
+        decimal b = 3;
+        decimal c = a + b;
+
+        return c;
+    }
+
+    public static int RunNumbers2()
+    {
+        int a = 5;
+        decimal b = 3;
+        decimal c = a + b;
+
+        return (int)c;
+    }
+
+    public static string RunText()
+    {
+        return "Den første returnerer decimal og den andre int";
+    }
+}
